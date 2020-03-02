fc01d2204250e164a33880719ff9cd1369edb5ec
destiaina <ina.destia1@gmail.com>
2020-03-02 10:48:40 +0700

    recyclerView

--- app/src/main/java/id/ac/polinema/recyclerview/models/TeamLogo.java
+++ app/src/main/java/id/ac/polinema/recyclerview/models/TeamLogo.java
@@ -1,5 +1,34 @@
 package id.ac.polinema.recyclerview.models;
 
-public class TeamLogo {
+import android.net.UrlQuerySanitizer;
 
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TeamLogo {
+    private String logo;
+    private String name;
+
+    public TeamLogo() {
+    }
+
+    public TeamLogo(String logo, String name) {
+        this.logo = logo;
+        this.name = name;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
